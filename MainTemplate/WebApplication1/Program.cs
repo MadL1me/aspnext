5cfd676a3710f03d056e1ea141f011daaf13d7c6
Ilya Klimenko <46647517+Sn0wy-0wl@users.noreply.github.com>
2022-06-01 14:12:01 +0300

    Added main template, created dotnet new creatable template

--- MainTemplate/WebApplication1/Program.cs
+++ MainTemplate/WebApplication1/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
