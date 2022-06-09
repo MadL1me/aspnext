78bd7dac7c3e655c142f4fa1f62760a97906f46c
Ilya Klimenko <46647517+Sn0wy-0wl@users.noreply.github.com>
2022-06-09 16:23:48 +0300

    feat: Made swagger support optional

--- Level X/Template/Program.cs
+++ Level X/Template/Program.cs
@@ -3,17 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+@*#if(EnableSwaggerSupport)
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+#endif*@
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+@*#if(EnableSwaggerSupport)
     app.UseSwagger();
     app.UseSwaggerUI();
+#endif*@
 }
 
 app.UseHttpsRedirection();
